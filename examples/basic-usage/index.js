7bdefa2621608ebfbc9014c7bfd4a5c6d8f1a02e
Alberto Mijares <almilo@almilo.es>
2016-09-04 03:07:49 +0200

    fix(basic-usage): use granate named import

--- examples/basic-usage/index.js
+++ examples/basic-usage/index.js
@@ -1,5 +1,5 @@
 import path from 'path';
-import granate from 'granate';
+import { granate } from 'granate';
 import { readFile, assertResponse } from '../lib';
 
 const schema = readFile(path.join(__dirname, './todos.graphql'));
